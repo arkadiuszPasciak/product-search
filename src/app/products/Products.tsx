9b12f941de886830bba67a5643a8e4004c17d758
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-03-17 18:35:17 +0100

    PS-0011: delete import UIInput

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -2,14 +2,11 @@ import { Link } from 'react-router-dom'
 
 import { AppRoute } from 'routing/AppRoute.enum'
 
-import { UIInput } from 'components/UI/UIInput/UIInput'
-
 export const Products = () => {
   return (
     <>
       <h2>Products page</h2>
       <Link to={AppRoute.Login}> Login </Link>
-      <UIInput name="name" color="primary" placeholder="placeholder text" />
     </>
   )
 }
