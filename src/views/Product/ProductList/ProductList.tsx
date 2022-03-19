f0c90d4e3e37a7e9ad4077ee6009ca43f50eddbf
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-03-19 17:58:20 +0100

    PS-0027: fixes styles body and header

--- src/views/Product/ProductList/ProductList.tsx
+++ src/views/Product/ProductList/ProductList.tsx
@@ -1,11 +1,12 @@
 import { Header } from 'components/Common/Header/Header'
 import { ProductList as ProductListComponent } from 'components/Product/ProductList/ProductList'
+import './ProductList.scss'
 
 export const ProductList = () => {
   return (
-    <>
+    <div className="ProductListRoute">
       <Header />
       <ProductListComponent />
-    </>
+    </div>
   )
 }
