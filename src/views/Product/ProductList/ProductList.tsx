8e0974fc34ffe51b374ca99f2e6d603e97ebdb84
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-03-19 11:16:07 +0100

    PS-0021: add component ProductList

--- src/views/Product/ProductList/ProductList.tsx
+++ src/views/Product/ProductList/ProductList.tsx
@@ -1,9 +1,11 @@
 import { Header } from 'components/Common/Header/Header'
+import { ProductList as ProductListComponent } from 'components/Product/ProductList/ProductList'
 
 export const ProductList = () => {
   return (
     <>
       <Header />
+      <ProductListComponent />
     </>
   )
 }
