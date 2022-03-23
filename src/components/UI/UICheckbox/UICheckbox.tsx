8deef51feef38e2e0e86c0a141d85bdce1ca80da
Arkadiusz Paściak <arkadiusz.pasciak@enp.pl>
2022-03-23 10:00:00 +0100

    PS-0035: set routing in component ProductSearch

--- src/components/UI/UICheckbox/UICheckbox.tsx
+++ src/components/UI/UICheckbox/UICheckbox.tsx
@@ -5,14 +5,23 @@ interface Props {
   name: string
   color: 'primary'
   id: string
+  checked: boolean
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export const UICheckbox = (props: Props) => {
-  const { name, color, id } = props
+  const { name, color, id, checked, onChange } = props
 
   return (
     <label className={`UICheckbox ${color}`} htmlFor={id}>
-      <input className="input" id={id} type="checkbox" name={name} />
+      <input
+        className="input"
+        id={id}
+        type="checkbox"
+        name={name}
+        defaultChecked={checked}
+        onChange={onChange}
+      />
 
       <span className="checkmark">
         <UIIcon name="checkmark" color="white" />
